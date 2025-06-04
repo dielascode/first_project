08ce16d4bb50fd3d5127f105b08975bc06ffa143
didil <faridfadilah70@gmail.com>
2025-06-04 10:56:36 +0700

    view data at views

--- catatan/views.py
+++ catatan/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Catatan
 # Create your views here.
+def beranda(request):
+    data = Catatan.objects.all().order_by('-tanggal')
+    return render(request, 'catatan/beranda.html', {'catatan':data})
