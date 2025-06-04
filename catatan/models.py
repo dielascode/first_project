beedb503b481cd891413eb828b35fdb326ce0c3c
didil <faridfadilah70@gmail.com>
2025-06-04 10:37:45 +0700

    make database

--- catatan/models.py
+++ catatan/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Catatan(models.Model):
+    judul = models.CharField(max_length=100) #ini untuk judul, teks pendek max 100 karakter
+    isi = models.TextField()
+    tanggal = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.judul
